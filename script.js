46e6191185abe9f322ffdafa4903f0430fcc9f7d
troysipp <sipprelle_troy@bah.com>
2017-10-26 16:16:11 -0400

    mvp done

--- script.js
+++ script.js
@@ -21,8 +21,10 @@ $(document).ready(function() {
   function createDiscs() {
     firstPole.append(smallDisc, mediumDisc, bigDisc);
   }
-
   createDiscs();
+
+  // Call all listener events
+
   selectFirstPole();
   selectSecondPole();
   selectThirdPole();
@@ -33,13 +35,6 @@ $(document).ready(function() {
   function selectFirstPole() {
     firstPole.on("click", function(e) {
       currentPole = this;
-      // if ($(e.target).hasClass("disc")) {
-      //   currentDisc = e.target;
-      // } else if ($(e.target).hasClass("pole")) {
-      // currentPole = e.target;
-      // }
-      // // set discNumber
-      // whichDiscNumber(currentDisc);
       if (previousPole === null && firstPoleList.length > 0) {
         currentNumber = firstPoleList[firstPoleList.length - 1];
         if ($(currentPole).hasClass("first")) {
@@ -48,11 +43,8 @@ $(document).ready(function() {
             .first();
         }
         previousPole = this;
-        // currentNumber = firstPoleList[firstPoleList.length - 1];
         console.log(currentNumber);
-        // firstPoleList.pop();
         previousPoleList = firstPoleList;
-        // console.log(secondPoleList[secondPoleList.length - 1]);
       } else {
         if (
           (previousPole !== null && firstPoleList.length === 0) ||
@@ -85,29 +77,10 @@ $(document).ready(function() {
       }
     });
   }
-  //if there's no input
-  /* make currentPole something
-make currentNumber something
-make currentDisc something */
-  //else there is an input
-  /* remove currentDisc from its parent div
-pop currentNumber from its old array
-Add it to its new array
-Append currentDisc to new pole
-reset currentPole to null
-reset currentNumber to null
-reset currentDisc to null */
+
   function selectSecondPole() {
     secondPole.on("click", function(e) {
       currentPole = this;
-      whichPole(currentPole);
-      // if ($(e.target).hasClass("disc")) {
-      //   // Abstract out as function which is current disc
-      // currentDisc = e.target;
-      // } else if ($(e.target).hasClass("pole")) {
-      //   currentPole = e.target;
-      // }
-      // // whichDiscNumber(currentDisc);
       if (previousPole === null && secondPoleList.length > 0) {
         if ($(currentPole).hasClass("second")) {
           currentDisc = $(".second")
@@ -118,8 +91,6 @@ reset currentDisc to null */
         }
         currentNumber = secondPoleList[secondPoleList.length - 1];
         previousPole = this;
-        // currentNumber = secondPoleList[secondPoleList.length - 1];
-        // secondPoleList.pop();
         previousPoleList = secondPoleList;
         console.log(currentNumber);
       } else {
@@ -161,13 +132,6 @@ reset currentDisc to null */
   function selectThirdPole() {
     thirdPole.on("click", function(e) {
       currentPole = this;
-      // if ($(e.target).hasClass("disc")) {
-      //   currentDisc = e.target;
-      // } else if ($(e.target).hasClass("pole")) {
-      // currentPole = e.target;
-      // }
-      // // set discNumber
-      // whichDiscNumber(currentDisc);
       if (previousPole === null && thirdPoleList.length > 0) {
         currentNumber = thirdPoleList[thirdPoleList.length - 1];
         if ($(currentPole).hasClass("third")) {
@@ -176,11 +140,8 @@ reset currentDisc to null */
             .first();
         }
         previousPole = this;
-        // currentNumber = firstPoleList[firstPoleList.length - 1];
         console.log(currentNumber);
-        // firstPoleList.pop();
         previousPoleList = thirdPoleList;
-        // console.log(secondPoleList[secondPoleList.length - 1]);
       } else {
         if (
           (previousPole !== null && thirdPoleList.length === 0) ||
@@ -199,6 +160,9 @@ reset currentDisc to null */
           previousPole = null;
           discNumber = null;
           previousPoleList = [];
+          if (thirdPoleList.length === 3) {
+            alert("You won the game!");
+          }
           console.log(thirdPoleList);
           console.log(thirdPoleList[thirdPoleList.length - 1]);
         } else {
@@ -212,53 +176,4 @@ reset currentDisc to null */
       }
     });
   }
-
-  // Select first pole
-  // If pole not empty, allow selection
-  // Select other pole
-  // If other pole has no numbers smaller than top of first pole, allow selection
-  // function move disc
-
-  function whichDiscNumber(currentDisc) {
-    if ($(currentDisc).hasClass("small")) {
-      discNumber = 1;
-      return currentDisc;
-    } else if ($(currentDisc).hasClass("medium")) {
-      discNumber = 2;
-      return currentDisc;
-    } else if ($(currentDisc).hasClass("big")) {
-      discNumber = 3;
-      return currentDisc;
-    } else {
-      return;
-    }
-  }
-
-  function whichPole(currentPole) {
-    if ($(currentPole).hasClass("first")) {
-      currentDisc = $(".first")
-        .children()
-        .first();
-      console.log(currentNumber);
-      console.log(currentDisc);
-    } else if ($(currentPole).hasClass("second")) {
-      // currentDisc = $(".second:first-child");
-      return currentNumber;
-    } else if ($(currentPole).hasClass("third")) {
-      currentNumber = thirdPoleList[thirdPoleList.length - 1];
-      currentDisc = $(".third:first-child");
-      return currentNumber;
-    } else {
-      return;
-    }
-  }
-  // if you click a disc
-  // its parent should lose that disc as a child element
 });
-
-// Smaller divs are unable to have larger divs stacked upon them
-//    If this is attempted, the div should remain at its previous position
-
-// Under that constraint, the user should be able to make any moves he/she chooses
-
-// If the user gets all three divs to the rightmost tower, he/she should get a display that they beat the game
